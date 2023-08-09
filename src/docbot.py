d0fc05402f5a21186f17de748c7155b3fb016aa6
Sebastian Moder <moder.sebastian@gmail.com>
2023-08-09 13:22:18 +0200

    creating logfiles in output folder

--- src/docbot.py
+++ src/docbot.py
@@ -13,26 +13,28 @@ from llama_index.retrievers import VectorIndexRetriever
 from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.indices.postprocessor import SimilarityPostprocessor
 from storageLogistics import build_new_storage
+import json
+import time
 
 
 # setup
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
+args = False
 # parse arguments
-parser = argparse.ArgumentParser(description='Script so useful.')
+parser = argparse.ArgumentParser()
 parser.add_argument("--build", type=bool, default=False)
 args = parser.parse_args()
 
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
 
 if args.build:
-    print("Building new storage...")
+    print("Building new storage...", flush=True)
     build_new_storage()
 else:
     # load vector store
-    print("Loading existing storage...")
-    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    print("Loading existing storage...", flush=True)
     index = load_index_from_storage(storage_context)
 
 
@@ -58,7 +60,7 @@ retriever = VectorIndexRetriever(
 
 # configure response synthesizer
 # After a retriever fetches relevant nodes, a BaseSynthesizer synthesizes the final response by combining the information.
-response_synthesizer = get_response_synthesizer()
+response_synthesizer = get_response_synthesizer(response_mode="refine")
 
 # assemble query engine
 # An index can have a variety of index-specific retrieval modes.
@@ -70,8 +72,41 @@ query_engine = RetrieverQueryEngine(
     node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)],
 )
 
+# prepare to save the output
+all_responses_dict = {}
+counter = 0
+timestamp = int(time.time())
+output_dir = os.makedirs("output", exist_ok=True)
+output_file = os.path.abspath(f"output/{timestamp}_chat_output.txt")
+
+
+# start the chat
 while True:
-    response = query_engine.query(input("User: "))
-    print(f"Agent: {response}")
-    for node in response.source_nodes:
-        print(f"Source node: {node}")
+    
+    my_query = input("User: ")
+    response = query_engine.query(my_query)
+    
+    print(f"Agent: {response.response}", flush=True)
+    print(f"Sources: {response.get_formatted_sources()}", flush=True)
+    
+    this_sources_dict = {}
+    for source in response.source_nodes:
+        this_sources_dict["id"] = source.node.node_id
+        this_sources_dict["text"] = source.node.text
+        this_sources_dict["score"] = source.score
+
+    this_response_dict = {}
+    this_response_dict = {
+    "query": my_query,
+    "response": response.response,
+    "sources": json.dumps(this_sources_dict),
+    }
+
+    all_responses_dict[counter] = this_response_dict
+    
+    with open(output_file, "w") as f:
+        json.dump(all_responses_dict, f)
+    
+    counter += 1
+
+
