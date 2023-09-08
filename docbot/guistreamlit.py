44f57c479e91693a1916b67f1a6abb9847ccad86
Sebastian Moder <moder.sebastian@gmail.com>
2023-09-08 12:50:14 +0200

    Set constants and initialize index and API key validation

--- docbot/guistreamlit.py
+++ docbot/guistreamlit.py
@@ -13,7 +13,7 @@ from llama_index.retrievers import VectorIndexRetriever
 from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.indices.postprocessor import SimilarityPostprocessor
 from storageLogistics import build_new_storage
-
+from llama_index.vector_stores import SimpleVectorStore
 import logging
 from localhelpers import (
     parse_response,
@@ -24,8 +24,8 @@ from localhelpers import (
 from langchain.chat_models import ChatOpenAI
 
 
-# load_dotenv()
-# openai.api_key = os.getenv("OPENAI_API_KEY")
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # setup logging
 logging.basicConfig(
@@ -34,20 +34,22 @@ logging.basicConfig(
     handlers=[logging.FileHandler("docbot.log"), logging.StreamHandler()],
 )
 
-# constants
+# Set constants
 STORAGE_FOLDER = "storage"
 OUTPUT_FOLDER = "output"
+STORAGE_TYPE = "simple"
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 
+# Define function to initialize the index
 @st.cache_resource
 def get_index():
-    index = load_index_from_storage(
-        StorageContext.from_defaults(persist_dir=f"{STORAGE_FOLDER}"),
-    )
+    storage_context = StorageContext.from_defaults(persist_dir=f"{STORAGE_FOLDER}/{STORAGE_TYPE}")
+    index = load_index_from_storage(storage_context=storage_context)
     return index
 
 
+# Define function to check whether the API key is valid
 def is_api_key_valid():
     try:
         response = openai.Completion.create(
@@ -58,8 +60,6 @@ def is_api_key_valid():
     else:
         return True
 
-
-# Initialize Streamlit
 
 if "openai" not in st.session_state:
     st.session_state["openai"] = None
@@ -133,8 +133,10 @@ if selected_option == "Manage":
 if selected_option == "Chat":
     st.title("Chat Interface")
     if st.session_state["openai"] is None:
-
-        st.warning('Please enter your OpenAI API Key in the sidebar and press "Update API Key".', icon="⚠️")
+        st.warning(
+            'Please enter your OpenAI API Key in the sidebar and press "Update API Key".',
+            icon="⚠️",
+        )
     else:
         st.subheader("LLM Settings")
         col_left, col_right = st.columns(2, gap="medium")
@@ -154,7 +156,6 @@ if selected_option == "Chat":
             )
 
         with col_right:
-
             top_k_nodes = st.number_input(
                 label="Similarity Top K",
                 min_value=1,
@@ -176,14 +177,13 @@ if selected_option == "Chat":
         user_input = st.text_input("Enter your message:", key="prompt")
 
         if st.button("Send"):
-            llm = OpenAI(
-                model=model, temperature=temperature, max_tokens=max_tokens
-            )
+            index = get_index()
+            llm = OpenAI(model=model, temperature=temperature, max_tokens=max_tokens)
 
             service_context = ServiceContext.from_defaults(llm=llm)
 
             retriever = VectorIndexRetriever(
-                index=get_index(), similarity_top_k=top_k_nodes
+                index=index, similarity_top_k=top_k_nodes
             )
 
             response_synthesizer = get_response_synthesizer(response_mode="refine")
@@ -191,9 +191,7 @@ if selected_option == "Chat":
             query_engine = RetrieverQueryEngine(
                 retriever=retriever,
                 response_synthesizer=response_synthesizer,
-                node_postprocessors=[
-                    SimilarityPostprocessor(similarity_cutoff=0.7)
-                ],
+                node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)],
             )
 
             with st.chat_message("User", avatar="🙋‍♂️"):
