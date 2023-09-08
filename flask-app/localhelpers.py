a5a9414d3230182b18c1db2377363e7e2380a1af
Sebastian Moder <moder.sebastian@gmail.com>
2023-09-08 12:50:43 +0200

    Add helper functions for parsing response, saving response to JSON, and displaying response and sources

---
+++ flask-app/localhelpers.py
@@ -0,0 +1,83 @@
+import os
+import streamlit as st
+import pandas as pd
+import json
+import time
+import pickle
+
+
+def parse_response(user_input, response):
+    # Parse response
+
+    this_sources_list = []
+    for source in response.source_nodes:
+        source_dict = {
+            "file_name": source.node.metadata["file_name"],
+            "similarity": source.score,
+            "node_id": source.node.node_id,
+            "text": source.node.text,
+        }
+        this_sources_list.append(source_dict)
+
+    parsed_response_dict = {
+        "message": user_input,
+        "response": response.response,
+        "sources": this_sources_list,
+    }
+
+    return parsed_response_dict
+
+
+def save_response_to_json(chat_response, OUTPUT_FOLDER):
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    output_json_file = os.path.join(OUTPUT_FOLDER, f"{timestamp}_chat_output.txt")
+
+    with open(output_json_file, "w") as f:
+        json.dump(chat_response, f)
+    return output_json_file
+
+
+def display_response(parsed_response_dict):
+    st.write(parsed_response_dict["response"])
+
+
+def display_sources(parsed_response_dict):
+    count = 1
+    for source in parsed_response_dict["sources"]:
+        with st.expander(
+            f"Source {count} Similarity: {round(source['similarity'], 5)}, File: "
+            + source["file_name"]
+        ):
+            st.write(f"Node-ID: {source['node_id']}")
+            st.write(f"{source['text']}")
+        count += 1
+
+
+def get_df_files(folder):
+    if not os.path.exists(folder):
+        st.write(f"The location '{folder}' does not exist.")
+        return
+
+    st.write(f"Files found at '{folder}': ")
+
+    files = os.listdir(folder)
+
+    file_info_list = []
+
+    for file_name in files:
+        file_path = os.path.join(folder, file_name)
+        file_size_bytes = os.path.getsize(file_path)
+        file_size_mb = file_size_bytes / 1024 / 1024
+        file_size = round(file_size_mb, 2)
+        file_creation_date = os.path.getctime(file_path)
+        file_info = {
+            "File Name": file_name,
+            "Size (MB)": file_size,
+            "Created Date": file_creation_date,
+        }
+        file_info_list.append(file_info)
+
+    # Create a DataFrame from the list of file info dictionaries
+    file_info_df = pd.DataFrame(file_info_list)
+
+    return file_info_df
---
+++ flask-app/storageLogistics.py
@@ -0,0 +1,86 @@
+import os
+from llama_index import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    ListIndex,
+    Document,
+    SimpleDirectoryReader,
+)
+from llama_index.node_parser import SimpleNodeParser
+from llama_index.storage.docstore import SimpleDocumentStore
+from llama_index.storage.index_store import SimpleIndexStore
+from llama_index.vector_stores import SimpleVectorStore
+from llama_index.text_splitter import TokenTextSplitter
+from llama_index.node_parser.extractors import (
+    MetadataExtractor,
+    SummaryExtractor,
+    QuestionsAnsweredExtractor,
+    TitleExtractor,
+    KeywordExtractor,
+    EntityExtractor,
+)
+
+
+def parse_docs_into_nodes(documents, chunk_size=300):
+    metadata_extractor = MetadataExtractor(
+        extractors=[
+            # TitleExtractor(nodes=5),
+            # QuestionsAnsweredExtractor(questions=3),
+            # SummaryExtractor(summaries=["prev", "self"]),
+            # KeywordExtractor(keywords=10),
+            # EntityExtractor(prediction_threshold=0.5),
+        ],
+    )
+
+    node_parser = SimpleNodeParser.from_defaults(
+        chunk_size=chunk_size,
+        metadata_extractor=metadata_extractor,
+    )
+
+    nodes = node_parser.get_nodes_from_documents(documents, show_progress=True)
+
+    return nodes
+
+
+def build_new_storage():
+    # load documents from directory
+    documents = SimpleDirectoryReader("documents/new", filename_as_id=True).load_data()
+    nodes = parse_docs_into_nodes(documents)
+    # parse document into nodes
+
+    # create storage context using default stores
+    storage_context = StorageContext.from_defaults(
+        docstore=SimpleDocumentStore(),
+        index_store=SimpleIndexStore(),
+        vector_store=SimpleVectorStore(),
+    )
+
+    # create (or load) docstore and add nodes
+    storage_context.docstore.add_documents(nodes)
+
+    # build index
+    vector_index = VectorStoreIndex(nodes, storage_context=storage_context)
+
+    # save index locally
+    # if you have multiple indices, add a uniqe id for each
+    # index.set_index_id = my_index_id
+    vector_index.storage_context.persist(persist_dir="./storage")
+
+    return vector_index
+
+
+# def insert_doc(document, index):
+#     storage_context = StorageContext.from_defaults(
+#         docstore=SimpleDocumentStore(),
+#         index_store=SimpleIndexStore(),
+#         vector_store=SimpleVectorStore(),
+#     )
+
+#     node_parser = set_node_parser(300)
+#     nodes = node_parser.get_nodes_from_documents(document)
+
+#     storage_context.docstore.add_documents(nodes)
+
+#     for node in nodes:
+#         index.insert(node)
